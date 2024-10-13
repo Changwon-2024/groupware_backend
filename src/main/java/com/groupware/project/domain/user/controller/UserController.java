ab28175893f33e603e50b3ef95442a5ffcd8d856
jys <ysjun1023@gmail.com>
2024-10-13 21:35:45 +0900

    [EDIT] Import문 최적화

--- src/main/java/com/groupware/project/domain/user/controller/UserController.java
+++ src/main/java/com/groupware/project/domain/user/controller/UserController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.media.SchemaProperties;
 import io.swagger.v3.oas.annotations.media.SchemaProperty;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
--- src/main/java/com/groupware/project/domain/user/service/UserService.java
+++ src/main/java/com/groupware/project/domain/user/service/UserService.java
@@ -14,7 +14,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
