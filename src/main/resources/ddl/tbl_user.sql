3a2a6e3b1da3bbd138857deb52969fcf0de1552c
jys <ysjun1023@gmail.com>
2024-10-13 15:55:57 +0900

    [EDIT] DDL 백업본 수정

--- src/main/resources/ddl/tbl_user.sql
+++ src/main/resources/ddl/tbl_user.sql
@@ -1,21 +1,20 @@
 create table if not exists tables.tbl_user
 (
-    user_key        uuid      default gen_random_uuid() not null
+    user_key       uuid       default gen_random_uuid()      not null
         constraint tbl_user_pk
             primary key,
-    email           varchar(100)                        not null,
-    password        varchar(100)                        not null,
-    name            varchar(10)                         not null,
-    phone_number    varchar(50),
-    address         varchar(100),
-    birth_date      date,
-    department_key  integer,
-    position_key    integer,
-    role_key        integer,
-    user_image_key  uuid,
-    stamp_image_key uuid,
-    reg_date_time   timestamp default CURRENT_TIMESTAMP not null,
-    is_deleted      boolean   default false             not null
+    email          varchar(100)                              not null,
+    password       varchar(100)                              not null,
+    name           varchar(10)                               not null,
+    phone_number   varchar(50),
+    address        varchar(100),
+    birth_date     date,
+    gender_flag    varchar(1) default 'M'::character varying not null,
+    note           varchar(1000),
+    role_level     integer    default 3                      not null,
+    user_image_key uuid,
+    reg_date_time  timestamp  default CURRENT_TIMESTAMP      not null,
+    is_deleted     boolean    default false                  not null
 );
 
 comment on table tables.tbl_user is '유저 테이블';
@@ -34,15 +33,13 @@ comment on column tables.tbl_user.address is '주소';
 
 comment on column tables.tbl_user.birth_date is '생일';
 
-comment on column tables.tbl_user.department_key is '부서 키';
+comment on column tables.tbl_user.gender_flag is '성별 구분 플래그(남성: M, 여성: F)';
 
-comment on column tables.tbl_user.position_key is '직위 키';
+comment on column tables.tbl_user.note is '비고';
 
-comment on column tables.tbl_user.role_key is '직책 및 권한 키';
+comment on column tables.tbl_user.role_level is '권한 단계 (0: 어드민, 1: 편집, 2: 조회 및 다운로드, 3: 가입대기)';
 
 comment on column tables.tbl_user.user_image_key is '유저 이미지 키';
-
-comment on column tables.tbl_user.stamp_image_key is '날인 이미지 키';
 
 comment on column tables.tbl_user.reg_date_time is '생성 시간';
 
--- src/main/resources/ddl/tbl_user_login.sql
+++ src/main/resources/ddl/tbl_user_login.sql
@@ -1,12 +1,12 @@
 create table if not exists tables.tbl_user_login
 (
-    user_key        uuid                                  not null
+    user_key        uuid                                    not null
         constraint tbl_user_login_tbl_user_user_key_fk
             references tables.tbl_user,
-    client_ip       char(15)  default '127.0.0.1'::bpchar not null,
-    token_value     char(175)                             not null,
-    login_date_time timestamp default CURRENT_TIMESTAMP   not null,
-    is_permitted    boolean   default true                not null,
+    client_ip       varchar(15) default '127.0.0.1'::bpchar not null,
+    token_value     varchar(250)                            not null,
+    login_date_time timestamp   default CURRENT_TIMESTAMP   not null,
+    is_permitted    boolean     default true                not null,
     constraint tbl_user_login_pk
         primary key (user_key, client_ip)
 );
